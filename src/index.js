08d9c8cbc7986b9a36d370e8bd5c634b1ac99f18
Noah Ekse <nolis1000@hotmail.com>
2025-02-04 10:22:58 +0100

    core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 
-console.log(data);
+const container = document.querySelector(".cards")
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+data.map((pokemon) => {
+    const card = document.createElement("li");
+    card.classList.add("card");
+    card.innerHTML = `
+        <h2 class="card--title">${capitalizeFirstLetter(pokemon.name)}</h2>
+        <img width="256" class="card--img" src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+    `;
+
+    const stats = document.createElement("ul");
+    stats.classList.add("card--stats");
+    const statTypes = ["hp", "attack", "defense", "special-attack", "special-defense", "speed"];
+    statTypes.map((type) => {
+        const stat = document.createElement("li");
+        stat.innerHTML = `
+            <span>${type.toUpperCase()}:</span>
+            <span>${pokemon.stats.find((s) => s.stat.name === type).base_stat}</span>
+        `;
+        stats.appendChild(stat);
+    });
+    
+    card.appendChild(stats);
+
+    container.appendChild(card);
+});
